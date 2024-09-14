4a80e8a9806c5d64c4af0780d3882fb79b244c3a
tr0fin0 <guitrofino@gmail.com>
2024-09-14 15:17:52 +0200

    [+] main.py file added

---
+++ src/main.py
@@ -0,0 +1,16 @@
+"""Main CSC_5RO17_TA_TP1 file."""
+
+# from q2 import q2
+# from q3 import q3
+# from q4 import q4
+
+
+def main():
+    """Save main TP functions."""
+    # q2()
+    # q3()
+    # q4()
+
+
+if __name__ == '__main__':
+    main()
